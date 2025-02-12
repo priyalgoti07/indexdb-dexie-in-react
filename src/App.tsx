e6483aeccb055f61659ef136917c7b91d12c604a
priyalgoti07 <gotipriyal1996@gmail.com>
2025-02-12 11:40:16 +0530

    both directional way implementationn

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import { Student } from './page/Student'
 import Child from './page/Child'
-
+//Both Parent to Child pass props and Child to Parent Communication 
 function App() {
   const [count, setCount] = useState(0)
   const inc = () => {
     setCount(count + 1)
   }
-
   return (
     <>
       <Student />
